0d5801ea723d5d34e3a65cfabc7603be62947b30
weexploit <1063915758@qq.com>
2018-04-07 21:42:59 +0800

    提交test

---
+++ 201708024204/src/CompleteNum/test.java
@@ -0,0 +1,15 @@
+package CompleteNum;
+
+import java.util.Scanner;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc=new Scanner(System.in);
+		int n=sc.nextInt();
+		CompleteNum ns=new CompleteNum();
+		ns.Func(n);
+	}
+
+}
